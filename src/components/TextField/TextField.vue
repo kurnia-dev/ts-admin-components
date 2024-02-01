d62b8e2b8a6648a24045532524f11609b193c58b
programmer-fe3 <programmer-fe3@qwertysystem.net>
2024-02-01 08:40:13 +0700

    style: add disabled style for input text

--- src/components/TextField/TextField.vue
+++ src/components/TextField/TextField.vue
@@ -3,27 +3,23 @@ import { onMounted, reactive } from 'vue';
 import { useField } from 'vee-validate';
 import { FieldValidation } from '@/types/fieldValidation.type';
 
-const props = withDefaults(
-  defineProps<{
-    modelValue?: string;
-    label: string;
-    fieldName?: string;
-    mandatory?: boolean;
-    useValidator?: boolean;
-    validatorMessage?: string;
-    placeholder?: string;
-    type?: 'email' | 'text';
-  }>(),
-  {
-    type: 'text',
-  }
-);
+const props = defineProps<{
+  modelValue?: string;
+  label?: string;
+  fieldName?: string;
+  mandatory?: boolean;
+  useValidator?: boolean;
+  validatorMessage?: string;
+  placeholder?: string;
+  type?: 'email' | 'text';
+  disabled?: boolean;
+}>();
 
 defineEmits<{
   'update:modelValue': [value?: string];
 }>();
 
-const field = reactive<FieldValidation>({});
+const field = reactive<FieldValidation>({ value: props.modelValue });
 
 onMounted(() => {
   if (props.useValidator) {
@@ -31,8 +27,8 @@ onMounted(() => {
       field,
       useField(props.fieldName ?? '', (value: string) => {
         if (props.mandatory) return setValidatorMessage(value);
-        else return true;
-      })
+        return true;
+      }),
     );
   }
 });
@@ -45,28 +41,39 @@ const setValidatorMessage = (value: string): boolean | string => {
   } else if (props.type === 'email') {
     const emailRegexp = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     return emailRegexp.test(value) ? true : 'Email format is incorrect!';
-  } else {
-    return true;
   }
+
+  return true;
 };
 </script>
 <template>
   <div class="field_wrapper">
     <label>
       {{ label }}
-      <span class="text-danger" v-if="mandatory">*</span>
+      <span v-if="mandatory" class="text-danger">*</span>
     </label>
     <div class="input_wrapper">
       <InputText
         v-model="field.value"
-        @update:modelValue="$emit('update:modelValue', $event)"
-        :placeholder="placeholder ?? `Input ${label.toLowerCase()}`"
         :class="[{ 'p-invalid': field.errorMessage }, 'w-100']"
+        :disabled="disabled"
+        :placeholder="
+          placeholder ?? `Input ${label ? label.toLowerCase() : 'text'}`
+        "
+        @update:model-value="$emit('update:modelValue', $event)"
         class="ts-inputtext"
       />
-      <small class="validator-message" id="dd-error" v-if="field.errorMessage">{{
-        field.errorMessage
-      }}</small>
+      <small id="dd-error" v-if="field.errorMessage" class="validator-message">
+        {{ field.errorMessage }}
+      </small>
     </div>
   </div>
 </template>
+<style lang="scss">
+@import 'var';
+
+.p-inputtext:disabled {
+  background-color: $general-input;
+  border-color: $general-body;
+}
+</style>
