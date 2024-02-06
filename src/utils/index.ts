24468db061c19fc839cf0350c98d4dc16558feac
programmer-fe3 <programmer-fe3@qwertysystem.net>
2024-02-06 14:11:40 +0700

    feat(util): add handle success and error

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
 import { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import exportToExcel from './exportToExcel.util';
+import { formatDate } from './date.util';
+import { formatUserName, formatVowelSoundLabel } from './textFormater.util';
+import { ToastServiceMethods } from 'primevue/toastservice';
 
 const isObjectEmpty = (obj: object) => {
   if (!obj) return true;
@@ -29,9 +33,49 @@ const getImgURL = (fileName: string) => {
   return `https://assets.tagsamurai.com/img/${fileName}`;
 };
 
+interface ToastParams {
+  toast: ToastServiceMethods;
+  activity: string;
+}
+
+type ErrorToastParams = ToastParams & { error: any };
+
+const handleError = (params: ErrorToastParams): void => {
+  const isNetworkError = params.error.code === 'ERR_NETWORK';
+  const additionalMsg = isNetworkError
+    ? ' Please check your connection again.'
+    : '';
+
+  let message = `Error, failed to ${params.activity}. ${additionalMsg}`;
+  if (params.error.response?.status === 400) {
+    message = params.error.response.data?.message;
+  }
+
+  params.toast.add({
+    severity: 'error',
+    detail: message,
+    life: 3000,
+  });
+};
+
+const handleSuccess = (params: ToastParams): void => {
+  const message = `Success, ${params.activity}.`;
+  params.toast.add({
+    severity: 'success',
+    detail: message,
+    life: 3000,
+  });
+};
+
 export {
   isObjectEmpty,
   addTokenExpirationHandling,
   filterSelectedTreeKeys,
   getImgURL,
+  exportToExcel,
+  formatDate,
+  formatUserName,
+  formatVowelSoundLabel,
+  handleError,
+  handleSuccess,
 };
